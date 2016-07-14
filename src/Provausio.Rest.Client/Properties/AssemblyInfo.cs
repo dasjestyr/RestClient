7c6f9d5398cbb4bac98d89ccf5aabdc6c8c14215
Jeremy Stafford <draepetan@live.com>
2016-07-13 22:54:35 -0700

    Added build incrementer and fixed assembly info

--- src/Provausio.Rest.Client/Properties/AssemblyInfo.cs
+++ src/Provausio.Rest.Client/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Provausio.RestClient")]
+[assembly: AssemblyTitle("Provausio.Rest.Client")]
 [assembly: AssemblyDescription("A REST wrapper for HttpClient with a fluid interface")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jeremy Stafford, Provausio")]
@@ -25,6 +25,6 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyVersion("0.6.0.*")]
 [assembly: AssemblyFileVersion("0.6.0.0")]
 [assembly: InternalsVisibleTo("Provausio.Rest.Client.Test")]
